4cc1bef731f80bc0d35ea7932e73fe08ca442bfe
Fabiano <fabiano.ssantos79@gmail.com>
2024-10-23 02:24:53 -0300

    feat: insert springdocs and swagger

--- src/main/java/com/exemplo/teste/TesteApplication.java
+++ src/main/java/com/exemplo/teste/TesteApplication.java
@@ -1,9 +1,12 @@
 package com.exemplo.teste;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product OpenApi", version = "1", description = "API desenvolvida para testes em java OpenApi"))
 public class TesteApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/exemplo/teste/controller/ProductController.java
+++ src/main/java/com/exemplo/teste/controller/ProductController.java
@@ -3,6 +3,10 @@ package com.exemplo.teste.controller;
 import com.exemplo.teste.domain.Product;
 import com.exemplo.teste.dto.RequestProductDTO;
 import com.exemplo.teste.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +18,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping(value = "/api/products", produces = {"application/json"})
+@Tag(name= "open-api")
 public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Operation(summary = "Busca no banco de dados todos os produtos com nome e preço", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso")
+    })
     @GetMapping
     public ResponseEntity<List<RequestProductDTO>> getAllProduct(){
 
