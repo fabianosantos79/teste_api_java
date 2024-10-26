ae8cdbced92c77eb95f5d563bf55db94929cd898
Fabiano <fabiano.ssantos79@gmail.com>
2024-10-26 03:20:30 -0300

    feat: update Migration - initial load - table products

---
+++ src/main/resources/db/migration/V2__initial-load-table-products.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (id, name, price) VALUES (1, "Microondas", 550);
+INSERT INTO products (id, name, price) VALUES (2, "Televisor", 3280);
+INSERT INTO products (id, name, price) VALUES (3, "Sofa", 1470);
+INSERT INTO products (id, name, price) VALUES (4, "Celular", 2125);
+INSERT INTO products (id, name, price) VALUES (5, "Liquidificador", 72);
+INSERT INTO products (id, name, price) VALUES (6, "Cama Box", 973);
