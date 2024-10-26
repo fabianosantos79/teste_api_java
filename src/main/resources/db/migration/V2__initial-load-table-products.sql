be060aea9b235be4291fdea7a8ee585ae10baf76
Fabiano <fabiano.ssantos79@gmail.com>
2024-10-26 03:37:02 -0300

    feat: update Migration - delete initial load

--- src/main/resources/db/migration/V2__initial-load-table-products.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO products (id, name, price) VALUES (1, "Aparelho", 550);
-INSERT INTO products (id, name, price) VALUES (2, "Televisor", 3280);
-INSERT INTO products (id, name, price) VALUES (3, "Sofa", 1470);
-INSERT INTO products (id, name, price) VALUES (4, "Celular", 2125);
-INSERT INTO products (id, name, price) VALUES (5, "Liquidificador", 72);
-INSERT INTO products (id, name, price) VALUES (6, "Cama Box", 973);
